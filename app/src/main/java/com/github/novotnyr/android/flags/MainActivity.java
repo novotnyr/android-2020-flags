19646652e8a514b24f1aef375cad53110cd8824c
Robert Novotny <novotnr0@gmail.com>
2020-02-22 00:13:19 +0100

    Pridany RecyclerView do layoutu

--- app/src/main/java/com/github/novotnyr/android/flags/MainActivity.java
+++ app/src/main/java/com/github/novotnyr/android/flags/MainActivity.java
@@ -1,14 +1,20 @@
 package com.github.novotnyr.android.flags;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView flagsRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flagsRecyclerView = findViewById(R.id.flagsRecyclerView);
+        flagsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
